d9cb3fadf5d4600c459d14c5115b115a09bb5712
See Hau Nan <haunan.see@kingmakergames.co>
2022-06-01 22:28:49 +0800

    add directory component & fonts

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
-import CategoryCard from "./components/category-card/category-card.component";
-
-import './categories.styles.scss';
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
   const categories = [
     {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-  ;
-
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryCard key={category.id} category={category} />
-      ))}
-    </div>
-  );
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return <Directory categories={categories} />;
 };
 
 export default App;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import CategoryCard from "../category-card/category-card.component";
+
+import "./directory.styles.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryCard key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
