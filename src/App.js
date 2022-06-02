49caf9fcdf1b555a047c51725daf4fc74953a345
Hau Nan See <haunansee@hotmail.com>
2022-06-02 23:15:58 +0800

    implement react-router-dom
    - add navigation component
    - implement Link, Outlet
    - import logo
    - add and update styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./components/routes/navigation/navigation.component";
 import Home from "./components/routes/home/home.component";
+
+
+const Shop = () => {
+  return <div>SHOP PAGE</div>
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />}/>
+        <Route path="shop" element={<Shop />}/>
+      </Route>
     </Routes>
   )
 };
---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import { ReactComponent as Logo } from "../../../assets/logo.svg";
+
+import "./navigation.styles.scss"
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <Logo />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            <div>Shop</div>
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
