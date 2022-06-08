965a8414f9a6ef0519ddfc3271c4da0e9f94b5b4
Hau Nan See <haunansee@hotmail.com>
2022-06-08 21:37:21 +0800

    add product-card component

---
+++ src/components/product-card/product-card.component.jsx
@@ -0,0 +1,33 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Stack from "@mui/material/Stack";
+
+import "./product-card.styles.scss";
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+
+  return (
+    <Card className="product-card-container" sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" height="300" image={imageUrl} alt={name} />
+      <CardContent>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography gutterBottom variant="h6" component="div">
+            {name}
+          </Typography>
+          <Typography variant="h6" gutterBottom component="div">
+            {`RM ${price}`}
+          </Typography>
+        </Stack>
+      </CardContent>
+      <Button size="large">Add to cart</Button>
+    </Card>
+  );
+};
+
+export default ProductCard;
