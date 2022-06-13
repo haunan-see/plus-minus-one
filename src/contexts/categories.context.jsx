999aa94561f154c7eeb6c91042747fea5096a3bb
Hau Nan See <haunansee@hotmail.com>
2022-06-13 21:52:30 +0800

    upload & fetch data from firebase
    - renamed product context

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+
+import { getCategoriesAndDocuments } from "../utilities/firebase/firebase.utilities";
+
+export const CategoriesContext = createContext({
+  categoriesMap: [],
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState([]);
+  const value = { categoriesMap };
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesMap = await getCategoriesAndDocuments();
+      
+      setCategoriesMap(categoriesMap);
+    }
+
+    getCategoriesMap();
+  }, [])
+
+  return (
+    <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+  );
+};
--- src/utilities/firebase/firebase.utilities.js
+++ src/utilities/firebase/firebase.utilities.js
@@ -9,7 +9,16 @@ import {
   GoogleAuthProvider,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -35,6 +44,21 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const batch = writeBatch(db);
+  const collectionRef = collection(db, collectionKey);
+
+  Array.from(objectsToAdd).forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+};
+
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocReference = doc(db, "users", userAuth.uid);
   const userSnapshot = await getDoc(userDocReference);
@@ -56,6 +80,21 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   }
 
   return userDocReference;
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories");
+  const q = query(collectionRef);
+
+  const querySnapShot = await getDocs(q);
+  const catergoriesMap = querySnapShot.docs.reduce((acc, docSnapShot) => {
+    const { title, items } = docSnapShot.data();
+    acc[title.toLowerCase()] = items;
+
+    return acc
+  }, {});
+
+  return catergoriesMap;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
