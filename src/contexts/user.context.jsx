b022d8483e3dadb60cf19614ffd8c74ba307331f
See Hau Nan <haunansee@hotmail.com>
2022-07-28 21:34:02 +0800

    add redux, react-redux, redux-logger & setup user redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,8 @@
-import { createContext, useEffect, useReducer } from "react";
-import { createAction } from "../utilities/reducer/reducer.utilities";
+import { createContext, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { setCurrentUser } from "../store/user/user.action";
+import { selectCurrentUser } from "../store/user/user.selector";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -7,44 +10,23 @@ import {
 
 export const UserContext = createContext({
   currentUser: null,
-  setCurrentUser: () => null,
 });
 
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
-
-const INITIAL_STATE = {
-  currentUser: null,
-};
-
-const userReducer = (state, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      throw new Error(`Unidentified USER_ACTION_TYPE: ${type}`);
-  }
-};
-
 export const UserProvider = ({ children }) => {
-  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
-  const setCurrentUser = (user) =>
-    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selectCurrentUser);
+  const value = { currentUser };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
-      setCurrentUser(user);
+      dispatch(setCurrentUser(user));
     });
 
     return unsubscribe;
   }, []);
-  const value = { currentUser };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
