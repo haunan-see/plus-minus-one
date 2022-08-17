5e7cbca7f8c602c01772c74b3f27763e03335733
See Hau Nan <haunansee@hotmail.com>
2022-08-17 21:47:05 +0800

    enable redux-devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,25 +1,33 @@
-import { compose, createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-// const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
+const composedEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     return next(action);
+//   }
 
-  next(action);
+//   console.log("type: ", action.type);
+//   console.log("payload: ", action.payload);
+//   console.log("currentState: ", store.getState());
 
-  console.log("next state: ", store.getState());
-};
+//   next(action);
+
+//   console.log("next state: ", store.getState());
+// };
 
 const persistConfig = {
   key: "root",
@@ -29,8 +37,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+// const middleWares = [loggerMiddleware];
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
